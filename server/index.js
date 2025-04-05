2046536e3f1ce99850287c2c98e12aeed439fabf
frontend-fuel <neverfail2026@gmail.com>
2025-04-05 20:14:17 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/smart-glasses')
+mongoose.connect(process.env.MONGODB_URI )
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
